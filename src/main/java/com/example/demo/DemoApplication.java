703b174ea8dac6b31535f996dc06d586d923c7d6
Tong Zhaoqi <1661610563@qq.com>
2023-09-18 13:00:46 +0000

    test

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController
+	public static class TestController {
+		@RequestMapping(value="/test" , method=RequestMethod.GET)
+		public String test() {
+			return "test\n";
+		}
+	}
+
+}
